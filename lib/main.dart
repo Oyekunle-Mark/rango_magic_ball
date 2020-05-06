b624b72aea1d80538da7bf2a3f3442dd8d2b5dc8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 16:59:13 +0100

    Added a bady to the scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.blue,
           appBar: AppBar(
             title: Text('Ask Me Anything'),
             backgroundColor: Colors.blue[900],
           ),
+          body: Container(),
         ),
       ),
     );
