bb0c1aef171df991ab6373200b126bab7222a575
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 17:11:42 +0100

    The state change triggers a change in image

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class _BallState extends State<Ball> {
             ballNumber = Random().nextInt(5) + 1;
           });
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
