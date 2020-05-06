882f1a290a6e16c11f11001f4fd85da2fdeb415f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 17:01:14 +0100

    Created a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,15 @@ void main() => runApp(
         ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
